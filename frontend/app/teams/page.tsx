bfb6eaf863f52a076bd95f1ceee2c80251fcfc0d
Ladislav Mozolic <tvoj@email.sk>
2026-07-17 14:40:07 +0200

    Teams loaded from PostgreSQL with Prisma

--- frontend/app/teams/page.tsx
+++ frontend/app/teams/page.tsx
@@ -1,11 +1,21 @@
+import TeamCard from "../../components/TeamCard";
 import { prisma } from "../../lib/prisma";
 
 export default async function TeamsPage() {
-  console.log(Object.keys(prisma));
+  const teams = await prisma.team.findMany();
 
   return (
     <main>
       <h1>Teams</h1>
+
+      {teams.map((team) => (
+        <TeamCard
+          key={team.id}
+          name={team.name}
+          gm={team.gm}
+          arena={team.arena}
+        />
+      ))}
     </main>
   );
 }
--- frontend/lib/prisma.ts
+++ frontend/lib/prisma.ts
@@ -1,11 +1,4 @@
-import { PrismaClient } from "../app/generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-
-const connectionString = process.env.DATABASE_URL!;
-
-const adapter = new PrismaPg({
-  connectionString,
-});
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
@@ -13,10 +6,8 @@ const globalForPrisma = globalThis as unknown as {
 
 export const prisma =
   globalForPrisma.prisma ??
-  new PrismaClient({
-    adapter,
-  });
+  new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
-}console.log(prisma);
+}
